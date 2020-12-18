50939ec229fd984237c3cafc7d28c0e4735afea9
lucaperullo <72197904+lucaperullo@users.noreply.github.com>
2020-12-18 15:33:11 +0100

    up

---
+++ src/fsUtilities.js
@@ -0,0 +1,28 @@
+const { readJSON, writeJSON } = require("fs-extra"); //ASYNC METHODS OF READING/WRITING JSONS
+const { join } = require("path"); //JOIN IS USED TO GET RELATIVE FILE PATHS
+const reviewPath = join(__dirname, "./reviews/reviews.json");
+const mediasPath = join(__dirname, "./media/media.json"); //CREATE A PATH TO medias JSON
+
+const readDB = async (filepath) => {
+  try {
+    const fileAsAJson = await readJSON(filepath);
+    return fileAsAJson;
+  } catch (riccky) {
+    throw new Error(riccky);
+  }
+};
+
+const writeDB = async (filepath, fileContent) => {
+  try {
+    await writeJSON(filepath, fileContent);
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+module.exports = {
+  readDB,
+  writeDB,
+  getmedias: async () => readDB(mediasPath), //EXPORTS A FUNCTION THAT AUTOMATICALLY RETURNS THE medias JSON AS AN ARRAY
+  writemedias: async (mediasData) => writeDB(mediasPath, mediasData), //EXPORTS A FUNCTION THAT ONLY RECIEVES AN ARRAY AND OVERWRITES THE medias JSON
+};
---
+++ src/media/index.js
@@ -0,0 +1,70 @@
+const { getmedias, writemedias } = require("../fsUtilities");
+const axios = require("axios");
+const express = require("express");
+const mediaRoute = express.Router();
+const { join } = require("path");
+const { check } = require("express-validator");
+// const PDFDocument = require("pdfkit")
+
+const mediasPath = join(__dirname, "media.json");
+// get medias from axios or from file with title
+mediaRoute.get("/", async (req, res, next) => {
+  try {
+    if (req.query && req.query.title) {
+      let response = axios
+        .get(`http://www.omdbapi.com/?apikey=bdddd0c1&t=${req.query.title}`)
+        .then((response) => res.send(response.data));
+    } else {
+      let medias = await getmedias(mediasPath);
+
+      res.send(medias);
+    }
+  } catch (err) {
+    console.log(err), next(err);
+  }
+});
+
+// get medias with id
+mediaRoute.get("/:id", async (req, res, next) => {
+  let response = axios
+    .get(`http://www.omdbapi.com/?apikey=bdddd0c1&i=${req.params.id}`)
+    .then((response) => res.send(response.data));
+});
+
+mediaRoute.post("/", async (req, res, next) => {
+  check("imdbID").exists().withMessage("imdbID is required").not().isEmpty();
+  try {
+    let newmedia = { ...req.body };
+    let medias = await getmedias(mediasPath);
+    medias.push(newmedia);
+    await writemedias(medias);
+    res.send("media posted correctly!");
+  } catch (err) {
+    console.log(err), next(err);
+  }
+});
+mediaRoute.put("/:id", async (req, res, next) => {
+  let medias = await getmedias(mediasPath);
+  let filteredmedia = medias.filter((media) => media.imdbID !== req.params.id);
+
+  editedmedia = { ...req.body, imdbID: req.params.id };
+  filteredmedia.push(editedmedia);
+  await writemedias(filteredmedia);
+  res.send("updated media");
+});
+
+mediaRoute.delete("/:id", async (req, res, next) => {
+  try {
+    let medias = await getmedias(mediasPath);
+    let filteredmedia = medias.filter(
+      (media) => media.imdbID !== req.params.id
+    );
+    await writemedias(filteredmedia);
+
+    res.send(filteredmedia);
+  } catch (err) {
+    console.log(err), next(err);
+  }
+});
+
+module.exports = mediaRoute;
